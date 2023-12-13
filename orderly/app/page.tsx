a710a887b7e9d505669fa222b9490584133a8b57
kodditor <kobbyowusudarko@gmail.com>
2023-12-13 00:19:43 +0000

    Made the main page responsive

--- orderly/app/page.tsx
+++ orderly/app/page.tsx
@@ -1,11 +1,14 @@
+import Header from "./components/Header.component";
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex flex-col gap-8 justify-center items-center">
-      <h1 className="text-5xl text-center font-black w-[55%]">Join hundreds of Ghanaian businesses elevating their online ordering experience with Orderly.</h1>
-      <a href="https://getwaitlist.com/waitlist/12417"><button>Get Orderly</button></a>
-      <small className="text-grey-200 font-italics">COMING SOON</small>
-    
-    </main>
+    <>
+      <Header />
+      <main className="w-screen h-[100%-4rem] pt-16 flex flex-col gap-8 justify-center items-center">
+        <h1 className="text-3xl md:text-5xl text-center font-black w-[80%] md:w-[55%]">Join hundreds of Ghanaian businesses elevating their online ordering experience with Orderly.</h1>
+        <a href="https://getwaitlist.com/waitlist/12417"><button>Get Orderly</button></a>
+        <small className="text-grey-200 font-italics">COMING SOON</small>
+      </main>
+    </>
   )
 }

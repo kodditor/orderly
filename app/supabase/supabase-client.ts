460d9a09f2cfac48b027773c26f2c168d6f7b6e8
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-01-23 02:17:32 +0000

    Added client side and server side db clients

--- app/supabase/supabase-client.ts
+++ app/supabase/supabase-client.ts
@@ -1,9 +1,10 @@
 "use client"
 
+import { Database } from '@/types/supabase'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from "next/headers"
 
-export const clientSupabase = createClientComponentClient({
+export const clientSupabase = createClientComponentClient<Database>({
     supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!, 
     supabaseKey: process.env.NEXT_PUBLIC_ANON_KEY!
 })
--- app/supabase/supabase-server.ts
+++ app/supabase/supabase-server.ts
@@ -1,7 +1,8 @@
+import { Database } from '@/types/supabase'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from "next/headers"
 
-export const serverSupabase = createServerComponentClient({ cookies },
+export const serverSupabase = createServerComponentClient<Database>({ cookies },
     {
     supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!, 
     supabaseKey: process.env.NEXT_PUBLIC_ANON_KEY!

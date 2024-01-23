c014bb2cbc9ede715eaafe4435336a690e2db257
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-01-23 02:20:21 +0000

    Updated Header

--- components/Header.component.tsx
+++ components/Header.component.tsx
@@ -1,9 +1,9 @@
-
+import Image from "next/image"
 
 export default function Header(){
     return (
-        <nav className="sticky top-0 z-40 flex items-center justify-between px-8 md:px-16 h-16 border-b-2 border-b-grey-200">
-            <h3 className="text-2xl font-bold">Orderly</h3>
+        <nav className="bg-white sticky top-0 z-40 flex items-center justify-between px-8 md:px-16 h-16 border-b-2 border-b-grey-200">
+            <span className="text-2xl font-bold"><Image src={'/img/logo.png'} width={175} height={50} alt={"The Orderly Logo"}/></span>
             <span>
                 <button>Get Orderly</button>
             </span>

1cd21975d26d18eb1b6bf83008dd22d3d29c19ba
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-01-23 02:20:10 +0000

    Added Footer

---
+++ components/Footer.component.tsx
@@ -0,0 +1,25 @@
+import { faInstagram, faFacebook, faXTwitter, faTiktok } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+
+export default function Footer(){
+    
+    let today = new Date()
+
+    return (
+        <>
+            <footer className="flex items-center justify-between p-5 z-50 relative bg-black w-[100%]">
+                <div><Image src={'/img/logo-white.png'} width={150} height={30} alt="The Orderly Logo" /> </div>
+                <div className="flex gap-4 items-center">
+                    <p className="text-center text-white pr-5 border-r-white border-r-[1.5px]">Orderly Ghana &copy; {today.getFullYear()}</p>
+                    <div className="flex gap-4 items-center">
+                        <a href="https://instagram.com/orderlyghana" className="bg-black hover:bg-white hover:text-black duration-150 cursor-pointer w-8 h-8 rounded-full flex items-center justify-center text-white p-2" ><FontAwesomeIcon icon={faInstagram} /></a>
+                        <a  className="bg-black hover:bg-white hover:text-black duration-150 cursor-pointer w-8 h-8 p-2 rounded-full flex items-center justify-center text-white" ><FontAwesomeIcon icon={faFacebook} /></a>
+                        <a  className="bg-black hover:bg-white hover:text-black duration-150 cursor-pointer w-8 h-8 p-2 rounded-full flex items-center justify-center text-white" ><FontAwesomeIcon icon={faXTwitter} /></a>
+                        <a  className="bg-black hover:bg-white hover:text-black duration-150 cursor-pointer w-7 h-7 p-2 rounded-full flex items-center justify-center text-white" ><FontAwesomeIcon icon={faTiktok} /></a>
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
+}

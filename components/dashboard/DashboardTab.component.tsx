9d815cb96aa147c1e3e5023710b43741cda980ee
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-02-06 00:37:44 +0000

    Added order load-in implementation

--- components/dashboard/DashboardTab.component.tsx
+++ components/dashboard/DashboardTab.component.tsx
@@ -3,18 +3,24 @@ import { clientSupabase } from "@/app/supabase/supabase-client"
 import type { RootState } from "@/constants/orderly.store"
 import Link from "next/link"
 import { useEffect, useState } from "react"
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Dispatch, SetStateAction } from "react"
 import { popupText } from "../Popup.component"
+import { setOrders } from "@/constants/orderly.slice"
+import { getOrdersWithProductsAndShopperDetails } from "@/app/utils/db/supabase-queries"
+import { pesewasToCedis } from "@/app/utils/frontend/utils"
 
 
 
 export default function DashboardTabComponent (){
 
     const {shop, user, orders, products} = useSelector((state: RootState) => state.shopAndUser)
+    const dispatch = useDispatch()
     const [ productsNum, setProductsNum ] = useState<number>(0)
     const [ ordersNum, setOrdersNum ] = useState<number>(0)
     const [ totalRevenue, setTotal ] = useState<number>(0)
+
+    const [ isLoadingOrders, setIsLoadingOrders ] = useState<boolean>(false)
 
     const today = new Date()
     let hour = today.getHours()
@@ -22,35 +28,6 @@ export default function DashboardTabComponent (){
     let timeOfDay = (hour < 12) ? 'Morning' : (hour < 17 ) ? 'Afternoon' : 'Evening'
 
     const supabase = clientSupabase
-
-    const orderTempObjects = [
-        {
-            id: '1',
-            products: 'Product 1, Product 4 and 13 others...',
-            amount: 275000,
-        },
-        {
-            id: '2',
-            products: 'Product 3, Product 2 and 1 other...',
-            amount: 200,
-        },
-        {
-            id: '3',
-            products: 'Product 6, Product 4 and 2 others...',
-            amount: 500,
-        },
-        {
-            id: '4',
-            products: 'Product 10, Product 42 and 7 others...',
-            amount: 7000,
-        },
-        {
-            id: '5',
-            products: 'Product 1 and Product 4',
-            amount: 275,
-        },
-    ]
-
     
     function getTableNum(table: string, setStateAction: Dispatch<SetStateAction<number>>){
         supabase
@@ -73,30 +50,26 @@ export default function DashboardTabComponent (){
     useEffect(() =>{
 
         if (products.length === 0){ getTableNum('products', setProductsNum)}
-        getTableNum('orders', setOrdersNum)
+        //getTableNum('orders', setOrdersNum)
 
         if (orders.length === 0){
             // Orders could also be zero because they have none... Just saying
-            supabase
-            .from('orders')
-            .select('total')
+            setIsLoadingOrders(true)
+            
+            getOrdersWithProductsAndShopperDetails
             .eq('shop_id', shop.id)
-            .then( ({data,error}) =>{
-                if(!error){
+            .then(({data, error}) =>{
+                if(error){
+                    console.log(error)
+                    popupText(`SB${error.code}: An error occured when fetching the orders`)
+                } else {
                     //console.log(data)
-                    if (data.length === 0){}
-                    else if (data.length === 1){ setTotal(data[1].total!)}
-                    else {
-                        let tempTotal = 0
-                        data.forEach((a)=>{
-                            tempTotal += a.total!
-                        })
-                        //console.log(tempTotal)
-                        setTotal(tempTotal)
-                    }
-                } else (
-                    console.error('Error: ', error)
-                )
+                    dispatch(setOrders(data))
+                    //console.log(data.length)
+                    setOrdersNum(data.length ?? 0)
+                }
+                setIsLoadingOrders(false)
+                //console.log(ordersNum)
             })
         }
 
@@ -122,7 +95,7 @@ export default function DashboardTabComponent (){
                             <small className="text-lg" >Active Orders</small>
                             <span className="flex gap-2 mt-1 md:mt-2 mb-2 items-baseline" >
                                 <h1 className="font-bold text-4xl" >{ordersNum}</h1>
-                                <h4 className="font-bold hidden md:block">orders</h4>
+                                <h4 className="font-bold hidden md:block">order(s)</h4>
                             </span>
                             <small className=" font-light" >this month</small>
                         </div>
@@ -141,21 +114,43 @@ export default function DashboardTabComponent (){
                     <h3 className="text-gray-500 mb-4">ACTIVE ORDERS</h3>
                     <div className="w-full border-2 border-peach rounded-xl">
                         <div className="bg-peach grid grid-cols-activeOrdersMob md:grid-cols-activeOrders gap-2 md:gap-4 p-2 md:p-4">
-                            <p className="hidden md:flex justify-center"><span className="hidden md:block">Order #</span><span className="block md:hidden">#</span></p>
+                            <p className="hidden md:flex justify-center">No.</p>
                             <p>Order Items</p>
                             <p className="hidden md:flex justify-center">Amount</p>
                             <p className="flex justify-center">Details</p>
                         </div>
                         <div className="max-h-[270px] overflow-auto">
-                            { orderTempObjects.map((order, idx) =>{
+                            { 
+                                isLoadingOrders && 
+                                    <>
+                                        <p className="text-gray-400 animate-pulse text-center py-4">Loading orders...</p>
+                                    </>
+                            }
+
+                            { 
+                                !isLoadingOrders && ordersNum == 0 &&
+                                    <>
+                                        <p className="text-gray-400 text-center py-4">You have no orders</p>
+                                    </>
+                            } 
+
+                            { !isLoadingOrders && ordersNum != 0 && orders.map((order, idx) =>{
+
+                                let total = 0
+                                for(let i = 0; i < order.order_products.length; i++){
+                                    total += order.order_products[i].price * order.order_products[i].quantity 
+                                }
+                                
                                 return (
-                                    <div className="border-b-peach last:border-b-transparent items-start md:items-center grid grid-cols-activeOrdersMob md:grid-cols-activeOrders gap-2 md:gap-4 p-2 md:p-4" key={idx}>
+                                    <div className="border-b-peach last:border-b-transparent items-start md:items-center grid grid-cols-activeOrdersMob md:grid-cols-activeOrders gap-2 p-2 md:p-4" key={idx}>
                                         <p className="hidden md:flex justify-center"># {order.id}</p>
-                                        <p>{order.products.length} Products
-                                        {/*<br /><span>Order Contact name</span>*/}
-                                        <br className="md:hidden" /><span className="md:hidden text-red font-bold">GHS{order.amount.toFixed(2)}</span></p>
-                                        <p className="font-black hidden md:flex justify-end">GHS{order.amount.toFixed(2)}</p>
-                                        <Link href={''} className="flex justify-center md:block"><button>View<span className="hidden md:inline"> Details</span></button></Link>
+                                        {/* @ts-ignore */}
+                                        <p>{order.shopper.firstName + ' ' + order.shopper.lastName + ' - ' + order.shopper.phone}
+                                            <br />
+                                            <span className="font-bold text-gray-500">{order.order_products.length } Products</span>
+                                            <span className="md:hidden text-red font-bold"> - GHS{pesewasToCedis(total).toFixed(2)}</span></p>
+                                        <p className="font-black hidden md:flex justify-center">GHS{pesewasToCedis(total).toFixed(2)}</p>
+                                        <Link href={`/s/dashboard?tab=orders&section=order&id=${order.id}`} className="flex items-center h-full md:h-fit md:items-start justify-center md:block"><button>View<span className="hidden md:inline"> Details</span></button></Link>
                                     </div>
                                 )
                             })

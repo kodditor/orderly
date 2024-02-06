b534c009774ff5d8956797acf3bfad50198d5039
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-02-06 00:34:15 +0000

    Added supabase query module

---
+++ app/utils/db/supabase-queries.tsx
@@ -0,0 +1,17 @@
+import { clientSupabase } from "@/app/supabase/supabase-client"
+import { QueryData } from "@supabase/supabase-js"
+
+export const getOrdersWithProductsAndShopperDetails = clientSupabase
+                .from('orders')
+                .select(`
+                    *, 
+                    shopper(*, location(*)), 
+                    order_products(*)
+                `)
+    
+
+export const getAllProducts = clientSupabase
+                            .from('products')
+                            .select('*')
+
+export type ordersType = QueryData<typeof getOrdersWithProductsAndShopperDetails>

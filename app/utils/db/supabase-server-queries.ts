6b102291a1b6cda65d55c7740c1073a58a1a78da
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-02-06 19:46:20 +0000

    Fixed typing issue

--- app/utils/db/supabase-server-queries.ts
+++ app/utils/db/supabase-server-queries.ts
@@ -1,11 +1,23 @@
 
 import { serverSupabase } from "@/app/supabase/supabase-server"
+import { IShop } from "@/models/shop.model"
 import { QueryData } from "@supabase/supabase-js"
 
-export const getShopDetails = serverSupabase
-                            .from('shops')
-                            .select(`
-                                *,
-                                location(*)
-                            `)
-export type shopDetailsType = QueryData<typeof getShopDetails>[0]
+export function getShopDetails(shopNameTag: string){ 
+    
+    return serverSupabase
+            .from('shops')
+            .select(`
+                *,
+                location (
+                    buildingNum,
+                    streetAddress,
+                    city,
+                    region,
+                    country
+                )
+            `)
+            .eq('shopNameTag', shopNameTag)
+            .returns<IShop>()
+}
+export type shopDetailsType = QueryData<typeof getShopDetails>

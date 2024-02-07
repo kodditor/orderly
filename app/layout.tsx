06e7443ed32d14ac1101608870868db6dde86193
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-02-07 11:48:27 +0000

    Updated font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Popup from '@/components/Popup.component'
 
+
 const metadata: Metadata = {
   title: 'Orderly Ghana',
   description: 'Orderly - The premier Ghanaian digital storefront solution.',
@@ -15,7 +16,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link href="https://fonts.cdnfonts.com/css/mona-sans" rel="stylesheet" /> 
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />

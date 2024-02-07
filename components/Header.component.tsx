6b97b8994fb6bb9efe49499f67d88abc47546b9e
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-02-07 11:49:37 +0000

    Minor fixes

--- components/Header.component.tsx
+++ components/Header.component.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Header(){
     return (
         <nav className="bg-white sticky top-0 z-40 flex items-center justify-between px-2 md:px-16 h-16 border-b-2 border-b-grey-200">
-            <span className="text-2xl font-bold"><Image src={'/img/logo.png'} width={175} height={50} alt={"The Orderly Logo"}/></span>
+            <Link href={'/'} className="text-2xl font-bold"><Image src={'/img/logo.png'} width={175} height={50} alt={"The Orderly Logo"}/></Link>
             <span>
                 <button>Get Orderly</button>
             </span>

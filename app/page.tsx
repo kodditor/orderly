0b099e928e734cce05318e83d7a8a3076a9a05ae
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-02-07 11:48:36 +0000

    Frontend issues

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 			<Header />
 			<main className="w-full h-[100%-4rem]">
 				<section className="flex bg-peach flex-col gap-4 justify-center py-6 md:py-16 w-[100%] px-[5%] md:px-[10%]  items-center">
-					<h1 className="text-3xl md:text-6xl text-center font-black w-[80%] md:w-[55%] text-darkRed">Discover the easiest way to sell online</h1>
+					<h1 className="text-3xl md:text-6xl text-center font-black w-[80%] md:w-[60%] text-darkRed">Discover the easiest way to sell online</h1>
 					<h3 className="text-xl md:text-2xl text-center w-[80%] md:w-[55%] text-darkRed">Orderly is the digital storefront solution that enables businesses to list their products and receive orders from online customers. Start selling or buying today!</h3>
 					<span className="flex flex-col items-center md:flex-row gap-2 md:gap-3">
 						<a href="https://getwaitlist.com/waitlist/12417">
@@ -27,7 +27,7 @@ export default function Home() {
 							<div className="w-full flex justify-center md:justify-start">
 								<small className="text-sm text-center md:text-left">CONVENIENCE</small>
 							</div>
-							<h1 className="text-darkRed leading-snug md:leading-normal text-4xl mt-4 mb-4 font-extrabold text-center md:text-left">Experience the Convenience of Setting Up Your Digital Storefront</h1>
+							<h1 className="text-red leading-snug text-4xl mt-4 mb-4 font-extrabold text-center md:text-left">Experience the Convenience of Setting Up Your Digital Storefront</h1>
 							<h4 className=" text-black text-2xl md:text-lg text-center md:text-left">Orderly provides a seamless solution for businesses to list their products and receive orders from online customers. With our user-friendly platform, you can easily set up your digital storefront and start selling in no time.</h4>
 						</div>
 						<div className="flex flex-col md:flex-row gap-8">
@@ -35,7 +35,7 @@ export default function Home() {
 								<span className="w-10 h-10 m-auto md:m-0 bg-peach rounded-full flex items-center justify-center">
 									<FontAwesomeIcon className="text-red" style={{width: '15px', height: '15px'}} icon={faHandsClapping} />
 								</span>
-								<h3 className="text-red text-2xl md:text-xl font-bold mb-2 mt-2 text-center md:text-left" >Easy Setup</h3>
+								<h3 className="text-darkRed text-2xl md:text-xl font-bold mb-2 mt-2 text-center md:text-left" >Easy Setup</h3>
 								<p className="text-black text-xl md:text-lg text-center md:text-left">Our platform allows you to quickly and effortlessly list your products for online sale.</p>
 							</div>
 							<div>
@@ -43,7 +43,7 @@ export default function Home() {
 									<FontAwesomeIcon className="text-red" style={{width: '15px', height: '15px'}} icon={faBoltLightning} />
 								</span>
 								
-								<h3 className="text-red text-2xl md:text-xl font-bold text-center mb-2 mt-2 md:text-left" >Efficient Orders</h3>
+								<h3 className="text-darkRed text-2xl md:text-xl font-bold text-center mb-2 mt-2 md:text-left" >Efficient Orders</h3>
 								<p className="text-black text-xl md:text-lg text-center md:text-left">Receive and manage orders from your customers with ease and efficiency.</p>
 							</div>
 						</div>
@@ -66,7 +66,7 @@ export default function Home() {
 							<div className="w-full flex justify-center md:justify-start">
 								<small className="text-sm text-center md:text-left">SIMPLIFIED</small>
 							</div>
-							<h1 className="text-darkRed leading-snug md:leading-normal text-4xl mt-4 mb-4 font-extrabold text-center md:text-left">List and sell your products online</h1>
+							<h1 className="text-red leading-snug text-4xl mt-4 mb-4 font-extrabold text-center md:text-left">List and sell your products online</h1>
 							<h4 className=" text-black text-2xl md:text-lg text-center md:text-left">Orderly provides an easy-to-use platform for businesses to list their products and receive orders from online customers. With our streamlined process, you can start selling your products online in no time.</h4>
 						</div>
 						<div className="flex flex-col md:flex-row gap-8">
@@ -74,7 +74,7 @@ export default function Home() {
 								<span className="w-10 h-10 m-auto md:m-0 bg-white rounded-full flex items-center justify-center">
 									<FontAwesomeIcon className="text-red" style={{width: '15px', height: '15px'}} icon={faHandsClapping} />
 								</span>
-								<h3 className="text-red text-2xl md:text-xl font-bold mb-2 mt-2 text-center md:text-left" >Easy Setup</h3>
+								<h3 className="text-darkRed text-2xl md:text-xl font-bold mb-2 mt-2 text-center md:text-left" >Easy Setup</h3>
 								<p className="text-black text-xl md:text-lg text-center md:text-left">Create your product listings and showcase them to a wide online audience.</p>
 							</div>
 							<div>
@@ -82,7 +82,7 @@ export default function Home() {
 									<FontAwesomeIcon className="text-red" style={{width: '15px', height: '15px'}} icon={faShieldAlt} />
 								</span>
 								
-								<h3 className="text-red text-2xl md:text-xl font-bold text-center mb-2 mt-2 md:text-left" >Secure Payments</h3>
+								<h3 className="text-darkRed text-2xl md:text-xl font-bold text-center mb-2 mt-2 md:text-left" >Secure Payments</h3>
 								<p className="text-black text-xl md:text-lg text-center md:text-left">Accept payments from customers securely through our integrated payment gateway.</p>
 							</div>
 						</div>
@@ -120,13 +120,13 @@ export default function Home() {
 							<small className="text-sm text-center">SIMPLIFIED</small>
 						</div>
 					</div>
-					<h1 className="text-darkRed leading-snug md:leading-normal text-4xl mt-4 mb-16 font-extrabold text-center">Effortless setup for expanding your market reach</h1>
+					<h1 className="text-darkRed leading-snug text-4xl mt-4 mb-16 font-extrabold text-center">Effortless setup for expanding your market reach</h1>
 					<div className="w-full flex flex-col md:flex-row gap-16 md:gap-8 mb-8">
 						<div className="w-full md:w-1/3">
 							<div className="w-full h-auto md:h-[250px] flex items-center justify-center rounded-lg overflow-hidden md:justify-start">
 								<img src="/img/support.png" />
 							</div>
-							<h1 className="text-red leading-snug md:leading-normal text-2xl mt-4 mb-4 font-extrabold text-center">Dedicated support for your business growth</h1>
+							<h1 className="text-red leading-snug text-2xl mt-4 mb-4 font-extrabold text-center">Dedicated support for your business growth</h1>
 							<h4 className=" text-black text-2xl md:text-lg text-center">Our team is committed to providing dedicated support to help your business thrive. We are here to assist you every step of the way.</h4>
 						</div>
 						
@@ -134,7 +134,7 @@ export default function Home() {
 							<div className="w-full h-auto md:h-[250px] flex items-center justify-center rounded-lg overflow-hidden md:justify-start">
 								<img src="/img/orders.png" />
 							</div>
-							<h1 className="text-red leading-snug md:leading-normal text-2xl mt-4 mb-4 font-extrabold text-center">Streamlined order management and fulfillment</h1>
+							<h1 className="text-red leading-snug text-2xl mt-4 mb-4 font-extrabold text-center">Streamlined order management and fulfillment</h1>
 							<h4 className=" text-black text-2xl md:text-lg text-center">With Orderly Ghana, you can easily manage and fulfill orders, ensuring a smooth and efficient process for your business.</h4>
 						</div>
 
@@ -142,7 +142,7 @@ export default function Home() {
 							<div className="w-full h-auto md:h-[250px] flex justify-center rounded-lg overflow-hidden md:justify-start">
 								<img src="/img/payments-2.png" />
 							</div>
-							<h1 className="text-red leading-snug md:leading-normal text-2xl mt-4 mb-4 font-extrabold text-center">Secure and reliable online payment processing</h1>
+							<h1 className="text-red leading-snug text-2xl mt-4 mb-4 font-extrabold text-center">Secure and reliable online payment processing</h1>
 							<h4 className=" text-black text-2xl md:text-lg text-center">Rest assured that your online payments are safe and secure with our trusted payment processing system.</h4>
 						</div>
 					</div>

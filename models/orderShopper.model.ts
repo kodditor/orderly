45319e7fb5e981b3a4eb4c35dde9dc813e24d4a6
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-02-11 21:34:14 +0000

    abstracted Location interface

--- models/orderShopper.model.ts
+++ models/orderShopper.model.ts
@@ -1,14 +1,16 @@
 export interface IShopperDetails {
     shopperFirstName: string,
     shopperLastName: string,
-    location: {
-        city: string,
-        buildingNum: string,
-        streetAddress: string,
-        region: string,
-        country: string
-      },
+    location: ILocation,
     phone: string,
     shopper_user_id?: string,
     email:string
+}
+
+export interface ILocation {
+    city: string,
+    buildingNum: string,
+    streetAddress: string,
+    region: string,
+    country: string
 }

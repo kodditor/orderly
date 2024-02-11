17d895de9e95709506355f333e650ab3ff90e957
kodditor <kobbyowusudarko@gmail.com>
2024-02-11 13:29:06 +0000

    Added hCaptcha + minor fixes to login ui

--- components/auth/Login.component.tsx
+++ components/auth/Login.component.tsx
@@ -4,6 +4,8 @@ import { useSearchParams, useRouter } from "next/navigation"
 import { FormEvent, useState } from "react"
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { clientSupabase } from "@/app/supabase/supabase-client"
+import { popupText } from "../Popup.component"
+import HCaptcha from "@hcaptcha/react-hcaptcha"
 
 /*
 * TODO
@@ -11,29 +13,35 @@ import { clientSupabase } from "@/app/supabase/supabase-client"
 * Add indicator
 */
 
+type loginDetails = {
+    email: string,
+    password: string
+}
+
 export default function LoginComponent(){
 
     const router = useRouter()
     const searchParams = useSearchParams()
 
-    const [ email, setEmail         ] = useState<string>('')
-    const [ pass, setPass           ] = useState<string>('')
     const [ submitted, setSubmitted ] = useState<boolean>(false)
+    const [ verified, setVerified   ] = useState<boolean>(false)
+
+    const [ loginDetails, setLoginDetails] = useState<loginDetails>({
+        email: "",
+        password: ""
+    })
 
     const supabase = clientSupabase
 
     let destination = searchParams.get('to')
 
     async function handleLoginSubmit(e: FormEvent){
-        
-        setSubmitted(true)
         e.preventDefault()
+        if(!verified) return
+        setSubmitted(true)
        
         let { data, error } = await supabase.auth.signInWithPassword(
-            {
-                email: email,
-                password: pass,
-            }
+            loginDetails
         )
 
         if (data.user){
@@ -41,13 +49,33 @@ export default function LoginComponent(){
             router.push( ( '/' + destination ) || '/s/dashboard')
 
         } else {
-            /*
-            * Show wrong password
-            * Ask to retry
-            * set submitted to false
-            */
-            console.log(error)
+            popupText('Invalid login details entered.')
+            //console.log(error)
+            setSubmitted(false)
         }
+    }
+
+    function handleValueChange(e: any){
+        let field = e.target.name
+        let value = e.target.value
+
+        switch (field) {
+            case "email":
+            case "password":
+                    setLoginDetails((prev) =>{
+                        return ({
+                            ...prev,
+                            [field]: value
+                        })
+                    })
+                break;
+        
+            default:
+                popupText(`An error occurred. ${field} is not a valid field`)
+                break;
+        }
+
+
     }
 
     return (
@@ -56,12 +84,22 @@ export default function LoginComponent(){
                 <form onSubmit={handleLoginSubmit} className="flex flex-col items-center gap-4 w-72 " >
                     <h6>Welcome Back.</h6>
                     <h1 className="text-3xl font-bold mb-4">Login</h1>
-                    <input className="p-2 pl-4 bg-peach rounded-full w-full" placeholder="Email Address" type="email" id='email' onChange={(e)=>{setEmail(e.target.value)}} required/>
-                    <input className="p-2 pl-4 bg-peach rounded-full w-full" placeholder="Password" type="password" id='pwd' onChange={(e)=>{setPass(e.target.value)}} minLength={8} required/>
-                    <button className="rounded-full w-full mb-4" disabled={submitted}>
-                            <div style={{display: submitted ? 'block' : 'none'}} id="loading"></div>
-                            <span style={{display: submitted ? 'none' : 'block'}} >Access Dashboard</span>
-                        </button>
+                    <span className="w-full flex flex-col gap-2">
+                        <label className="text-sm" htmlFor="name">Email Address</label>
+                        <input className="p-2 pl-4 bg-peach rounded-full w-full" placeholder="kwaku@ananse.com" type="email" name="email" id='email' onChange={handleValueChange} required/>
+                    </span>
+                    <span className="w-full flex flex-col gap-2 mb-2">
+                        <label className="text-sm" htmlFor="name">Password</label>
+                        <input className="p-2 pl-4 bg-peach rounded-full w-full" placeholder="superSecretPassword" type="password" id='password' name="password" onChange={handleValueChange} minLength={8} required/>
+                    </span>
+                    <button className="rounded-full w-full mb-4" disabled={submitted || !verified}>
+                        <div style={{display: submitted ? 'block' : 'none'}} id="loading"></div>
+                        <span style={{display: submitted ? 'none' : 'block'}} >Access Dashboard</span>
+                    </button>
+                    <HCaptcha
+                        sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY!}
+                        onVerify={() => setVerified(true)}
+                    />
                     <h3>Don't have an account? <a href={`/auth/signup?to=${destination || '/s/dashboard' }`} className=" text-red cursor-pointer underline-offset-1 hover:underline-offset-2 duration-150 underline">Sign Up</a></h3>
                     <a href={`/auth/forgot?to=${destination}`} className="cursor-pointer no-underline hover:underline duration-150"><h4>Forgot Password?</h4></a>
                 </form>

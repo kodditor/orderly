a9c6d8f450dc34741501c388000653d6f2fe741f
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-02-19 12:50:46 +0000

    Email fix

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -16,12 +16,14 @@ export async function POST(req: Request) {
 
     const { email, parameters } = await req.json() as unknown as confirmPostBody
 
+    console.log(email, parameters)
+
     const resendResponse= await resend.emails.send({
     from: 'Orderly Ghana <noreply@orderlygh.shop>',
     to: [email],
     subject: `Order #${parameters.order_id} confirmed!`,
     react: ConfirmEmail({parameters}),
     });
+    console.log(resendResponse)
     return NextResponse.json({resendResponse})
-
 }

083d755cf3ccf79e4ccb22afb38b185b4550e293
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-02-22 00:31:12 +0000

    Fixed shop loading ui for mobile

--- components/shop/ShopSidebar.component.tsx
+++ components/shop/ShopSidebar.component.tsx
@@ -23,29 +23,26 @@ export default function ShopSideBar({ showCart, setShowCart, cart }:
     if(shop.name == ''){
         return (
             <>
-                <aside  className="hidden md:block z-30 fixed p-5 pt-8 bg-white w-[20vw] xl:w-[15vw] min-w-[240px] h-[calc(100%-72px)] md:top-[63px] min-h-[500px] border-r-2 border-grey-200 overflow-auto ">
+                <aside  className="block z-30 fixed p-5 pt-8 bg-white w-full md:w-[20vw] 2xl:w-[15vw] min-w-[240px] h-fit md:h-[calc(100%-72px)] md:top-[63px] min-h-[500px] border-r-2 border-grey-200 overflow-auto ">
                     <div className="flex flex-col gap-2 animate-pulse">
-                        <div className=" aspect-square w-1/3 md:w-full md:max-w-[200px] mb-4 flex items-center rounded-full border-2 border-peach justify-center">
-                            <img className="w-full saturate-0 animate-pulse" src={'/img/logo.png'} alt="The Orderly logo"/>
+                        <div className="flex flex-row items-center md:flex-col gap-6 md:gap-4">
+                            <div className=" aspect-square w-1/3 md:w-full md:m-auto md:max-w-[200px] mb-4 flex items-center rounded-full border-2 border-peach justify-center">
+                                <img className="w-full saturate-0 animate-pulse" src={'/img/logo.png'} alt="The Orderly logo"/>
+                            </div>
+                            <div className="w-2/3 md:w-full flex flex-col gap-2">
+                                <div className="w-full h-4 bg-gray-300 rounded-full"></div>
+                                <div className="w-2/3 h-4 mb-2 bg-gray-300 rounded-full"></div>
+                            
+                                <div className="w-3/4 h-3 mb-6 bg-gray-300 rounded-full"></div>
+                            </div>
                         </div>
-                        <div className="w-full h-5 bg-gray-300 rounded-full"></div>
-                        <div className="w-2/3 h-5 mb-2 bg-gray-300 rounded-full"></div>
-                        
-                        <div className="w-3/4 h-3 mb-6 bg-gray-300 rounded-full"></div>
-                        
                         <div className="w-full h-4 bg-gray-300 rounded-full"></div>
                         <div className="w-full h-4 bg-gray-300 rounded-full"></div>
                         <div className="w-full h-4 bg-gray-300 rounded-full"></div>
                         <div className="w-3/4 h-3 bg-gray-300 rounded-full mb-6"></div>
                         
-                        <div className="flex justify-evenly mb-6">
-                            <span className="bg-gray-300 inline-block w-1/5 h-6 rounded-md"></span>
-                            <span className="bg-gray-300 inline-block w-1/5 h-6 rounded-md"></span>
-                            <span className="bg-gray-300 inline-block w-1/5 h-6 rounded-md"></span>
-                            <span className="bg-gray-300 inline-block w-1/5 h-6 rounded-md"></span>
-                        </div>
                         <button disabled > <FontAwesomeIcon className="mr-1" icon={faPhone} /> Contact Business</button>
-                        <button disabled > <FontAwesomeIcon className="mr-1" icon={faShoppingBasket} /> View Cart</button>
+                        <button className="hidden md:block" disabled > <FontAwesomeIcon className="mr-1" icon={faShoppingBasket} /> View Cart</button>
                     </div>
                 </aside>
             </>
@@ -53,7 +50,7 @@ export default function ShopSideBar({ showCart, setShowCart, cart }:
     } else {
         return (
             <>
-                <aside  className="block md:fixed p-4 md:p-5 md:pt-8 md:pb-2 z-30 bg-white w-full md:w-[20vw] xl:w-[15vw] md:min-w-[240px] md:h-[calc(100%-60px)] md:top-[63px] md:min-h-[500px] border-b-2 md:border-r-2 border-grey-200 overflow-auto ">
+                <aside  className="block md:fixed p-4 md:p-5 md:pt-8 md:pb-2 z-30 bg-white w-full md:w-[20vw] 2xl:w-[15vw] md:min-w-[240px] md:h-[calc(100%-60px)] md:top-[63px] md:min-h-[500px] border-b-2 md:border-r-2 border-grey-200 overflow-auto ">
                     <div className="flex flex-col" >
                         <div className="flex flex-row md:flex-col items-center gap-4 md:gap-2" >
                             <div className="aspect-square w-1/3 md:w-full md:max-w-[200px] md:m-auto mb-4 flex items-center rounded-full overflow-hidden border-2 border-peach justify-center">

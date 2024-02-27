401cd74e7c7fa19be0636d4834912b550e07b6d8
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-02-27 21:09:11 +0000

    Added shop reporting

--- components/shop/ShopSidebar.component.tsx
+++ components/shop/ShopSidebar.component.tsx
@@ -2,9 +2,9 @@
 
 import { RootState } from "@/constants/orderly.store"
 import { IShopCart } from "@/models/OrderProducts.model"
-import { faPhone, faShoppingBasket, faShoppingCart } from "@fortawesome/free-solid-svg-icons"
+import { faEllipsisV, faPhone, faShoppingBasket, faShoppingCart } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { Dispatch, SetStateAction } from "react"
+import { Dispatch, SetStateAction, useRef, useState } from "react"
 import { useSelector } from "react-redux"
 
 
@@ -17,8 +17,13 @@ export default function ShopSideBar({ showCart, setShowCart, cart }:
     ){
 
     const {shop} = useSelector((state: RootState) => state.shopAndUser) 
-
+    const reportRef = useRef<HTMLDialogElement>(null)
+    const [reportReason, setReportReason] = useState<string>("")
     //console.log(shop)
+
+    function handleReportStore(){
+         throw new Error("Report function not implemented")
+    }
 
     if(shop.name == ''){
         return (
@@ -41,8 +46,8 @@ export default function ShopSideBar({ showCart, setShowCart, cart }:
                         <div className="w-full h-4 bg-gray-300 rounded-full"></div>
                         <div className="w-3/4 h-3 bg-gray-300 rounded-full mb-6"></div>
                         
-                        <button disabled > <FontAwesomeIcon className="mr-1" icon={faPhone} /> Contact Business</button>
-                        <button className="hidden md:block" disabled > <FontAwesomeIcon className="mr-1" icon={faShoppingBasket} /> View Cart</button>
+                        <button disabled > <FontAwesomeIcon className="mr-1" width={15} height={15} icon={faPhone} /> Contact Business</button>
+                        <button className="hidden md:block" disabled > <FontAwesomeIcon className="mr-1" width={15} height={15} icon={faShoppingBasket} /> View Cart</button>
                     </div>
                 </aside>
             </>
@@ -50,6 +55,17 @@ export default function ShopSideBar({ showCart, setShowCart, cart }:
     } else {
         return (
             <>
+                <dialog ref={reportRef} className="w-[90%] md:max-w-[400px] rounded-xl overflow-hidden">
+                    <form onSubmit={handleReportStore} className="p-6 flex flex-col gap-3">
+                        <h1 className="text-xl font-semibold">Report {shop.name}?</h1>
+                        <p>This will notify us of any inappropriate or illegal behaviour by the store.</p>
+                        <textarea className="p-2 bg-peach rounded-xl w-full" rows={3} placeholder="Please provide details on the reasons behind your report" required onChange={(e) => {setReportReason(e.target.value)}}/>
+                        <div className="flex gap-2 mt-2">
+                            <button className="w-1/2" >Submit Report</button>
+                            <button className="btn-secondary w-1/2" onClick={(e)=>{ e.preventDefault(); reportRef.current?.close()}}>Cancel</button>
+                        </div>
+                    </form>
+                </dialog>
                 <aside  className="block md:fixed p-4 md:p-5 md:pt-8 md:pb-2 z-30 bg-white w-full md:w-[20vw] 2xl:w-[15vw] md:min-w-[240px] md:h-[calc(100%-60px)] md:top-[63px] md:min-h-[500px] border-b-2 md:border-r-2 border-grey-200 overflow-auto ">
                     <div className="flex flex-col" >
                         <div className="flex flex-row md:flex-col items-center gap-4 md:gap-2" >
@@ -71,7 +87,19 @@ export default function ShopSideBar({ showCart, setShowCart, cart }:
                            
                         </small> */} 
                         <span className="flex flex-col gap-4 mt-4">
-                            <button className="btn-secondary mt-3 md:mt-0 w-fit md:w-full"> <FontAwesomeIcon className="mr-1" width={15} height={15} icon={faPhone} /> Contact Shop</button>
+                            <span className="mt-3 md:mt-0 w-full flex items-center md:items-baseline gap-4 md:gap-2">
+                                <button className="btn-secondary w-fit md:w-[calc(100%-30px-0.5rem)]"> <FontAwesomeIcon className="mr-1" width={15} height={15} icon={faPhone} /> Contact Shop</button>
+                                <div className=" hidden group h-[40px] relative w-[40px] md:grid place-items-center text-red cursor-pointer border-red border-2 rounded-full">
+                                    <FontAwesomeIcon width={15} height={15} icon={faEllipsisV} />
+                                    <div className="absolute top-9 right-2 w-[130px] rounded-xl overflow-hidden hidden group-hover:flex flex-col bg-white border-2 border-gray-100">
+                                        <div className="px-3 py-2 hover:bg-peach hover:text-darkRed duration-150 bg-white w-full" onClick={()=>{reportRef.current?.showModal()}}>Report shop</div>
+
+                                    </div>
+                                </div>
+                                <p className="md:hidden text-red font-medium" onClick={()=>{reportRef.current?.showModal()}}>
+                                    Report Shop
+                                </p>
+                            </span>
                             <button className="hidden md:flex items-center justify-center gap-2 mt-3 md:mt-0 w-fit md:w-full" onClick={()=>setShowCart(true)}> <FontAwesomeIcon className="mr-1" width={15} height={15} icon={faShoppingCart} /> My Cart ({cart.products?.length})</button>
                         </span>
                     </div>

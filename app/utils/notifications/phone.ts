9033f3581ffdb1b0718dd52ca8533b7b82182516
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-02-27 21:09:31 +0000

    Created catchall sms function

--- app/utils/notifications/phone.ts
+++ app/utils/notifications/phone.ts
@@ -105,3 +105,53 @@ export async function sendDeclinedText(number:string, order_id:string | number, 
     return response
 
 } 
+
+export async function sendText(number:string, message: string): Promise<notificationResponse> {
+
+    let response: notificationResponse
+
+    if(number.startsWith('0')){
+        number = '233' + number.slice(1)
+    }
+
+    const data = {
+        "sender": "Orderly GH",
+        "message": `${message}`,
+        "recipients": [number],
+        //"sandbox": true
+    }
+
+    let res = await fetch('https://sms.arkesel.com/api/v2/sms/send',
+    {
+        method: 'post',
+        headers: {
+            'api-key': process.env.NEXT_PUBLIC_ARKESEL_API_KEY!,
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+    let resData = await res.json()
+    if(resData.status == 'success'){
+        response = {
+            data: {
+                status: '200',
+                msg: 'Message sent successfully'
+            },
+            error: null
+        }
+    } else {
+        response = { 
+            data: {
+                status: '200',
+                msg: 'Message sent successfully'
+            },
+            error: {
+                code: '402',
+                msg: 'An error occurred'
+            }
+        }
+    }
+    
+    return response
+
+} 

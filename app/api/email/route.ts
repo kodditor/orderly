8a35cd4033a96b70ee156399bf9bfd77e2ba50a0
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-02-28 23:17:06 +0000

    Changed to Mailersend

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,16 +1,17 @@
 import { ConfirmEmail } from "@/app/utils/notifications/templates/emails.template";
 import { emailConfirmationParameters} from "@/models/notification.model";
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
-
-
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import { render } from '@react-email/render';
+import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
 
 type confirmPostBody ={
     email: string,
     parameters: emailConfirmationParameters
 }
+
+const mailerSend = new MailerSend({
+  apiKey: process.env.MAILERSEND_API_KEY || '',
+});
 
 export async function POST(req: Request) {
 
@@ -18,12 +19,19 @@ export async function POST(req: Request) {
 
     console.log(email, parameters)
 
-    const resendResponse= await resend.emails.send({
-    from: 'Orderly Ghana <noreply@orderlygh.shop>',
-    to: [email],
-    subject: `Order #${parameters.order_id} confirmed!`,
-    react: ConfirmEmail({parameters}),
-    });
-    console.log(resendResponse)
-    return NextResponse.json({resendResponse})
+    const emailHtml = render(ConfirmEmail({parameters}));
+
+    const sentFrom = new Sender("noreply@orderlygh.shop", "Orderly Ghana");
+    const recipients = [
+    new Recipient(email, parameters.firstName)
+    ];
+
+    const emailParams = new EmailParams()
+    .setFrom(sentFrom)
+    .setTo(recipients)
+    .setSubject(`Order #${parameters.order_id} confirmed!`)
+    .setHtml(emailHtml)
+
+    const mailSenderResponse = await mailerSend.email.send(emailParams);
+    return NextResponse.json(mailSenderResponse)
 }

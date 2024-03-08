f3fd46bd8af59131ae725996274fc455a1427fc9
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-03-08 13:29:24 +0000

    Updated toaster

--- components/Popup.component.tsx
+++ components/Popup.component.tsx
@@ -1,15 +1,31 @@
-import { faInfo } from "@fortawesome/free-solid-svg-icons/faInfo";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { POPUP_ICON } from "@/models/popup.enum";
+import { POPUP_STATE } from "@/models/popup.enum";
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/ReactToastify.css' ;
 
-export function popupText(text:string, state?: POPUP_ICON){
-    const popupText = document.getElementById("popup-text")!
-    const popupContainer = document.getElementById("popup-container")!
-    popupText.innerHTML = text
-    popupContainer.style.display = 'block'
-    setTimeout( ()=>{ popupContainer.style.display = 'none'}, 4000)
-    // solution to changing the icon might be through accessing the i class field and changing the classnames (since fa uses the fa-{item} syntax) 
+export function popupText(text:string, state?: POPUP_STATE){
+    if(state == undefined){
+        toast(text)
+    }
+
+    switch (state) {
+        case POPUP_STATE.SUCCESS:
+            toast.success(text)
+            break;
+        case POPUP_STATE.FAILED:
+            toast.error(text)
+            break;
+
+        case POPUP_STATE.INFO:
+            toast.info(text)
+            break;
+
+        case POPUP_STATE.WARNING:
+            toast.warning(text)
+            break;
+        default:
+            break;
+    }
 
 }
 
@@ -17,14 +33,18 @@ export default function Popup (){
 
     return(
         <>
-            <div className="fixed w-[80%] flex items-center z-[99] ml-10 md:w-fit bottom-5 md:bottom-10 md:right-10 md:left-auto"  style={{display:'none'}} id="popup-container">
-                <div className="flex gap-2 items-start md:items-center border-2 bg-white border-darkRed px-3 py-2 rounded-full">
-                    <div className="w-[20px] animate-pulse h-[20px] rounded-full bg-darkRed flex items-center justify-center">
-                        <FontAwesomeIcon className="w-1 text-white" icon={faInfo} />
-                    </div>
-                    <p className="w-[calc(100%-28px)]" id="popup-text">This is a popup!</p>                    
-                </div>
-            </div>
+            <ToastContainer
+                position="bottom-right"
+                autoClose={4000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </>
     )
 
